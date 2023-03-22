dfc51017ae3af98423ddaa85c9fa40d55f50d736
IseKaiBear <geolyun@gmail.com>
2023-03-22 16:08:03 +0900

    Add files via upload

---
+++ 7. 소수(에라토스테네스 체)/소수 정답.py
@@ -0,0 +1,15 @@
+n = int(input())
+ch = [0]*(n+1)
+cnt = 0
+for i in range(2, n+1):
+    if ch[i] == 0:
+        cnt += 1
+        for j in range(i, n+1, i):
+            ch[j]=1
+
+print(cnt)
+
+"""
+복습할 때 다시 한번 배수로 풀어보기
+그림으로도 그려보기
+"""
