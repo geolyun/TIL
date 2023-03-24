5e7ecf3d1ab3bd2c2b01a9a1c43e603bf0079072
IseKaiBear <geolyun@gmail.com>
2023-03-24 21:14:22 +0900

    Add files via upload

---
+++ 섹션2/9. 주사위 게임/주사위 게임.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+m = 0
+for i in range(n):
+  a, b, c, = map(int, input().split())
+  if a == b == c:
+    money = 10000 + (a*1000)
+  elif a == b:
+    money = 1000 + (a*100)
+  elif b == c:
+    money = 1000 + (b*100)
+  elif a == c:
+    money = 1000 + (a*100)
+  else:
+    money = max(a,b,c) * 100
+
+  if money > m:
+    m = money
+
+print(m)
