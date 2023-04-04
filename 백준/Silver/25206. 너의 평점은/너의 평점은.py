34eb4244cfd51a29d5eded5c04b0f0a754e3e0bc
CutyTeddyBear <geolyun@gmail.com>
2023-04-04 20:21:12 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,16 @@
+score = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+
+sum = 0
+n = 0
+
+for i in range(20):
+    N, S, H = input().split()
+    if H == "P":
+        continue
+    else:
+        s = float(S)
+        h = score.get(H)
+        sum += (s*h)
+        n += s
+
+print(f"{sum/n:.6f}") 
