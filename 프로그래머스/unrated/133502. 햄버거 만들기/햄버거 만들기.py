f47094d5ef3deddf6c86ddaf49d6eb7ea6993f61
CutyTeddyBear <geolyun@gmail.com>
2023-05-08 00:59:34 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,12 @@
+def solution(ingredient):
+    answer = 0
+    ham = [1,2,3,1]
+    s = -1
+    
+    while s < len(ingredient)-4:
+        s += 1
+        if ingredient[s:s+4] == ham:
+            del ingredient[s:s+4]
+            answer += 1
+            s = s-4
+    return answer
