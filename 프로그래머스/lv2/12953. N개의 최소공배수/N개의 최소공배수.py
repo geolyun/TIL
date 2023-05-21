12ae894c8015591401694cb3349efb7a2f0b1b30
CutyTeddyBear <geolyun@gmail.com>
2023-05-21 18:27:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -2,6 +2,8 @@ def solution(arr):
     answer = 0
     
     for i in range(len(arr)-1):
+        # 리스트에서 2개씩 출력해서 최소공배수를 구한 후 다시 리스트에 삽입
+        # 리스트 길이가 1이 되면 그때 그 값 출력
         a = arr.pop()
         b = arr.pop()
         ab = a*b
