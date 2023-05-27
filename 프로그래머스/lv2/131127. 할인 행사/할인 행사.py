9adf8a7c8b2610865e8db7fbd7afb3eadc8f4f00
CutyTeddyBear <geolyun@gmail.com>
2023-05-27 16:38:35 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -3,9 +3,11 @@ def solution(want, number, discount):
     cnt = sum(number)
     
     for i in range(len(discount)-cnt+1):
+        # 리스트의 길이가 물품 수와 같을 때까지만 리스트를 만들면 됨
         ord = discount[i:i+cnt]
         cnt_list = []
         for j in want:
+            # discount에서 number의 수만큼 뽑은 리스트에서 want에 있는 요소의 개수를 센 뒤 number과 같으면 답에 +1을 해줌
             cnt_list.append(ord.count(j))
         if cnt_list == number:
             answer += 1
