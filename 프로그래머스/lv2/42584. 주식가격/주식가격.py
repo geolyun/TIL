59ae8dcc5586a0329c9175c2694b2b3f9487d233
CutyTeddyBear <geolyun@gmail.com>
2023-06-02 20:02:20 +0900

    [level 2] Title: 주식가격, Time: 69.72 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -4,6 +4,7 @@ def solution(prices):
     
     while len(prices) > 0:
         cnt = 0
+        # list의 pop(0)의 시간복잡도는 O(n)이어서 O(1)의 시간복잡도를 가지는 pop(-1) 또는 pop()로 풀어야했음
         n = prices.pop()
         for i in range(len(prices)-1, -1, -1):
             cnt += 1
