bbffa01c3baade0c82c891311010213a348828ae
CutyTeddyBear <geolyun@gmail.com>
2023-06-06 19:55:42 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,15 +2,18 @@ def solution(skill, skill_trees):
     answer = 0
     arr = []
     
+    # skill의 최대 길이가 크지 않으므로 가능한 스킬트리들을 전부 리스트에 추가했음
     for i in range(len(skill)):
         arr.append(skill[0:i+1])
     
     for i in skill_trees:
+        # 스킬트리 리스트를 돌면서 스킬 문자열 안에 있으면 문자열 변수에 추가
         tree = ""
         for j in i:
             if j in skill:
                 tree += j
+        # 스킬과 관련이 없는 공백 문자열도 가능한 스킬트리
         if tree in arr or len(tree) == 0:
             answer += 1
-    print(arr)
+
     return answer
