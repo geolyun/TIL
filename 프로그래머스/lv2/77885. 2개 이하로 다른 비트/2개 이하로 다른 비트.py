ef6f7b47561a0f1f9a7c9c0ea499902c48ce709b
CutyTeddyBear <geolyun@gmail.com>
2023-07-04 06:39:29 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 201.87 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,5 +1,8 @@
 def solution(numbers):
     answer = []
+    # 짝수라면 가장 뒤에 있는 0을 1로 바꾸어주면 조건을 만족함
+    # 홀수라면 가장 뒤에 있는 0을 1로 바꾸고 그 다음 인덱스를 0으로 바꾸어주면 조건을 만족함
+    # 규칙을 찾으면 쉽게 풀렸음
     for number in numbers:
         bin_number = list('0' + bin(number)[2:])
         idx = ''.join(bin_number).rfind('0')
